99608d81ab3ebb7bf20a5e7d569140650ebe590b
Shaan-somaiah <shaansomaiah@gmail.com>
2026-05-25 15:00:51 +0530

    Add optional dependency for glog, gflags and grpc

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -9,7 +9,7 @@ option(USE_GFLAGS "Use gflags for command-line flag parsing" ON)
 option(USE_GRPC "Use gRPC for remote procedure calls" ON)
 
 ## Check if required packages are already installed, if not complile from source
-
+## My internal cpp libraries 
 if(USE_MY_CPP_LIBRARIES)
     if(USE_SYSTEM_PACKAGES)
         find_package(cppLibraries CONFIG QUITE)
@@ -17,6 +17,9 @@ if(USE_MY_CPP_LIBRARIES)
 
     if(NOT cppLibraries_FOUND)
         message(STATUS "cppLibraries not installed locally, building from source!!")
+
+        ## Disable generating test binary 
+        set(CPP_LIBRARIES_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 
         FetchContent_Declare(
             cppLibraries
@@ -29,5 +32,83 @@ if(USE_MY_CPP_LIBRARIES)
 
     else()
         message(STATUS "Using system installed cppLibraries!")
+    endif()
+endif()
+
+## Google gflags library 
+if(USE_GFLAGS)
+    if(USE_SYSTEM_PACKAGES)
+        find_package(gflags CONFIG QUITE)
+    endif()
+
+    if(NOT gflags_FOUND)
+        message(STATUS "gflags not installed locally, building from source!!")
+
+        FetchContent_Declare(
+            gflags
+            GIT_REPOSITORY https://github.com/gflags/gflags.git
+            GIT_TAG v2.3.0
+            GIT_SHALLOW TRUE
+        )
+
+        FetchContent_MakeAvailable(gflags)
+
+    else()
+        message(STATUS "Using system installed gflags!")
+    endif()
+endif()
+
+## Google logging library 
+if(USE_GLOG)
+    if(USE_SYSTEM_PACKAGES)
+        find_package(glog CONFIG QUITE)
+    endif()
+
+    if(NOT glog_FOUND)
+        message(STATUS "glog not installed locally, building from source!!")
+
+        FetchContent_Declare(
+            glog
+            GIT_REPOSITORY https://github.com/google/glog.git
+            GIT_TAG v0.7.1
+            GIT_SHALLOW TRUE
+        )
+
+        FetchContent_MakeAvailable(glog)
+
+    else()
+        message(STATUS "Using system installed glog!")
+    endif()
+endif()
+
+## Google RPC library + protobuf
+## Generating code from proto defined in grpc.cmake
+if(USE_GRPC)
+    if(USE_SYSTEM_PACKAGES)
+        find_package(Protobuf CONFIG QUITE)
+        find_package(gRPC CONFIG QUITE)
+    endif()
+
+    if(NOT Protobuf_FOUND OR NOT gRPC_FOUND)
+        message(STATUS "gRPC/Protobuf not installed locally, building from source!!")
+
+        ## This parameter enables propagating C++ features to targets that link to this package's libraries
+        set(ABSL_PROPAGATE_CXX_STD ON CACHE BOOL "" FORCE)
+
+        ## Disable tests for protobuf and grpc
+        set(protobuf_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+        set(gRPC_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+
+        FetchContent_Declare(
+            grpc
+            GIT_REPOSITORY https://github.com/grpc/grpc.git
+            GIT_TAG v1.64.0
+            GIT_SHALLOW TRUE
+        )
+
+        FetchContent_MakeAvailable(grpc)
+
+    else()
+        message(STATUS "Using system installed gRPC/Protobuf!")
     endif()
 endif()
